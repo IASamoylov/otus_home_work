56142f4a7d871b684281dabb93996de9d2d8f6d8
ivan.samoylov <samoylovia@DESKTOP-9J64BOR.localdomain>
2021-03-06 22:35:15 +0300

    - fixed tests

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello!")
+	runes := []rune("Hello, OTUS!")
+	iS, iE := 0, len(runes)-1
+
+	for iS < iE {
+		runes[iS] = (runes[iS] ^ runes[iE])
+		runes[iE] = (runes[iS] ^ runes[iE])
+		runes[iS] = (runes[iS] ^ runes[iE])
+		iS++
+		iE--
+	}
+
+	fmt.Println(string(runes))
 }
