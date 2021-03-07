a32f8b3160c4df78b8f32d403607274b24df7f1a
ivan.samoylov <samoylovia@DESKTOP-9J64BOR.localdomain>
2021-03-07 11:35:49 +0300

    add external lib

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -2,19 +2,10 @@ package main
 
 import (
 	"fmt"
+
+	"golang.org/x/example/stringutil"
 )
 
 func main() {
-	runes := []rune("Hello, OTUS!")
-	iS, iE := 0, len(runes)-1
-
-	for iS < iE {
-		runes[iS] = (runes[iS] ^ runes[iE])
-		runes[iE] = (runes[iS] ^ runes[iE])
-		runes[iS] = (runes[iS] ^ runes[iE])
-		iS++
-		iE--
-	}
-
-	fmt.Println(string(runes))
+	fmt.Println(stringutil.Reverse("Hello, OTUS!"))
 }
