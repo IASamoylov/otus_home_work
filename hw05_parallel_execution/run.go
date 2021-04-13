82edc370540ae450c4a19245de7e04fd79a55dbd
ivan.samoylov <ia.samoylov@outlook.com>
2021-04-13 23:26:07 +0300

    hw05 implemented task processor

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,6 +2,7 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
@@ -10,6 +11,73 @@ type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+	if len(tasks) == 0 {
+		return nil
+	}
+
+	countOfTasks := len(tasks)
+
+	tasksChannel := make(chan Task, len(tasks))
+	errorChannel := make(chan error, len(tasks))
+	shutdownChannel := make(chan interface{})
+
+	var wg sync.WaitGroup
+
+	for i := n; i != 0; i-- {
+		wg.Add(1)
+		go startConsumer(&wg, shutdownChannel, tasksChannel, errorChannel)
+	}
+
+	produce(tasks, tasksChannel)
+
+	for err := range errorChannel {
+		if err != nil {
+			m--
+		}
+
+		countOfTasks--
+
+		if m == 0 {
+			forceClose(&wg, shutdownChannel, tasksChannel, errorChannel)
+			return ErrErrorsLimitExceeded
+		}
+
+		if countOfTasks == 0 {
+			forceClose(&wg, shutdownChannel, tasksChannel, errorChannel)
+			return nil
+		}
+	}
+
 	return nil
+}
+
+func forceClose(wg *sync.WaitGroup, shutdownChannel chan interface{}, tasksQueue chan Task, errorChannel chan error) {
+	close(shutdownChannel)
+	close(tasksQueue)
+	wg.Wait()
+	close(errorChannel)
+}
+
+func produce(tasks []Task, tasksChannel chan Task) {
+	for _, t := range tasks {
+		tasksChannel <- t
+	}
+}
+
+func startConsumer(wg *sync.WaitGroup, shutdownChannel chan interface{}, tasksQueue chan Task, errorChannel chan error) {
+	defer wg.Done()
+
+	for {
+		select {
+		case <-shutdownChannel:
+			return
+		default:
+		}
+
+		f, ok := <-tasksQueue
+		if ok {
+			err := f()
+			errorChannel <- err
+		}
+	}
 }
