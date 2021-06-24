a938af857c90d34f6a48576bb72413dee7d71c08
Samoylov Ivan <ivasamoylov@ozon.ru>
2021-06-25 01:08:48 +0300

    stats_optimization_test.go:44: time used: 173.278062ms/300ms     stats_optimization_test.go:45: memory used: 9Mb/30Mb

--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -21,41 +21,62 @@ type User struct {
 type DomainStat map[string]int
 
 func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
-	u, err := getUsers(r)
+	u, err := getUsers(r, domain)
 	if err != nil {
 		return nil, fmt.Errorf("get users error: %w", err)
 	}
-	return countDomains(u, domain)
+	return countDomains(u[:len(u)])
 }
 
-type users [100_000]User
+type users []User
 
-func getUsers(r io.Reader) (result users, err error) {
+func getUsers(r io.Reader, domain string) (result users, err error) {
+	result = make(users, 0)
+
 	scanner := bufio.NewScanner(r)
-	i := 0
 	var user *User
+	b := []byte("." + domain + "\",\"Phone\"")
 	for scanner.Scan() {
-		if err = json.Unmarshal(scanner.Bytes(), &user); err != nil {
-			return
+		if isSubArray(scanner.Bytes(), b, len(scanner.Bytes()), len(b)) {
+			if err = json.Unmarshal(scanner.Bytes(), &user); err != nil {
+				return
+			}
+
+			result = append(result, *user)
 		}
-		result[i] = *user
-		i++
 	}
 
 	err = scanner.Err()
 	return
 }
 
-func countDomains(u users, domain string) (DomainStat, error) {
+func countDomains(u []User) (DomainStat, error) {
 	result := make(DomainStat)
 
 	for _, user := range u {
-		if strings.Contains(user.Email, domain) {
-			d := strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])
-			num := result[d]
-			num++
-			result[d] = num
-		}
+		d := strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])
+		result[d]++
 	}
 	return result, nil
+}
+
+func isSubArray(a, b []byte, n, m int) bool {
+	i := 0
+	j := 0
+
+	for i < n && j < m {
+		if a[i] == b[j] {
+			i++
+			j++
+
+			if j == m {
+				return true
+			}
+		} else {
+			i = i - j + 1
+			j = 0
+		}
+	}
+
+	return false
 }
